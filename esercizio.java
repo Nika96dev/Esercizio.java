ab9710d8e4e434d8416b1679424e6666376f3483
Nika96dev <pierpaolodagostino@gmail.com>
2025-05-03 15:39:30 +0200

    esercizio variabili

---
+++ esercizio.java
@@ -0,0 +1,20 @@
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		int rosso = 4;
+        //tipo variabile, nome variabile, operazione, valore;
+        float blu = 4.1f;
+        String giallo = "uno";
+        //chiamata del metodo o funzione
+        System.out.println(rosso); 
+
+        if(rosso != blu){ 
+            System.out.println("no");
+            }
+        else{ 
+            System.out.println(rosso == blu);
+            }
+}
+}
+
